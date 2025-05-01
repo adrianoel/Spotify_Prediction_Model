63af17b44b5497a5f2fb0af06f8207d99744e41f
Adriano Elia <adrianoeljero@gmail.com>
2025-05-01 09:36:17 +0200

    emptied the file so python can use it as a package identifier for the folder

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-## to do: to be changed so that __init__.py changes the src folder to a python package
-
-def main():
-    print("Hello from spotify-recommend-model!")
-
-
-if __name__ == "__main__":
-    main()
