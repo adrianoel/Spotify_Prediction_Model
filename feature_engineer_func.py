d4be11a4c58ce301b37731f1da434e19207ab3a9
Adriano Elia <adrianoeljero@gmail.com>
2025-05-06 11:37:54 +0200

    First version of feature engineering function (only new columns, no fit-transform stuff, no onehotencoding at this stage)

--- feature_engineer_func.py
+++ feature_engineer_func.py
@@ -1,17 +1,19 @@
 # This script is used to create new columns or changed columns due to feature engineering
 # It can be used on train, test and validation/aim dataframes because there are no fit-transformer methods used
-# one hot encoding, etc. will be used later in the pipeline step, not here
+# one hot encoding, etc. will not be used here, but later in the pipeline step
 
 import pandas as pd
 
 def feature_engineer(df_input):
     '''Feature engineering by creating new columns that are more suitable for machine learning models.
+    They are derived from categorical columns with too many unique values.
     New features:
-        - artist_popularity: Mean track popularity per artist
-        - 
+        - artist_popularity: Mean track popularity per artist (by grouping 'artists' and 'popularity')
+        - album_popularity: Mean track popularity per album (by grouping 'album' and 'popularity')
+        - track_name_length: Length of the track name (by getting the length of the strings in 'track_name')
 
     Args:
-        df_input (pd.DataFrame): The input DataFrame to feature engineer.
+        df_input (pd.DataFrame): The (cleaned) input DataFrame to feature engineer.
         
     
     Returns:
@@ -21,26 +23,16 @@ def feature_engineer(df_input):
     # copy input dataframe first
     df = df_input.copy()
 
-    # remove unnecessary column if it exists
-    if 'Unnamed: 0' in df.columns:
-        df = df.drop(columns=['Unnamed: 0'])
+    # create artist_popularity feature
+    artist_popularity = df.groupby('artists')['popularity'].mean().to_dict()
+    df['artist_popularity'] = df['artists'].map(artist_popularity)
 
-    # remove nan values
-    df = df.dropna()
+    # create album_popularity feature
+    album_popularity = df.groupby('album_name')['album_name'].mean().to_dict()
+    df['album_popularity'] = df['album_name'].map(album_popularity)
+    
 
-    # remove duplicates
-    relevant_cols = ['artists', 'track_name', 'duration_ms', 'explicit',
-                     'danceability', 'energy', 'key', 'loudness', 'mode',
-                     'speechiness', 'acousticness', 'instrumentalness',
-                     'liveness', 'valence', 'tempo', 'time_signature'] 
-    df = df.drop_duplicates(subset=relevant_cols)
-
-    # adjust feature type (from bool to int, True = 1, False = 0)
-    df['explicit'] = df['explicit'].astype(int)
-
-    # Use pd.cut to categorize popularity into four ranges (unknown = -1-0, low = 1-25, medium = 26-74, high = 75-100)
-    df['popularity_cat'] = pd.cut(df['popularity'],
-                                        bins=[-1, 0, 25, 74, 100],
-                                        labels=['Unknown', 'Low', 'Medium', 'High'])
+    # create track_name_length feature
+    df['track_name_length'] = df['track_name'].str.len()
 
     return df
